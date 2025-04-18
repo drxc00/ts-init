1d11088da6a86ce400d592049ce94b8ff26685cb
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-04-18 10:40:36 +0800

    refactor: enhance documentation and error handling in initNpmApp and createTsConfigFile functions

--- package/index.ts
+++ package/index.ts
@@ -3,202 +3,212 @@ import { Command } from "commander";
 import inquirer from "inquirer";
 import chalk from "chalk";
 import { createTsConfigFile, initNpmApp } from "./scripts.js";
-import type { ConfigOptions, TsConfig } from "./types.js";
+import type { ConfigOptions } from "./types.js";
 
-const program = new Command();
-program
-  .name("ts-init")
-  .description("Yet another typescript tool.")
-  .version("1.0.0")
-  .option("-n, --name <name>", "Project name")
-  .option("-d, --description <description>", "Project description")
-  .option(
-    "-t, --transpile <transpile>",
-    "Do you want to transpile your project?"
-  )
-  .option("-o, --output-dir <outputDir>", "What is your output directory?")
-  .option("-l, --library <library>", "Are you building a library?")
-  .option("-m, --monorepo <monorepo>", "Is the library for a monorepo?")
-  .option("-b, --dom <dom>", "Will your app be running in the browser?")
-  .action(async (options) => {
-    try {
-      /**
-       * @type {ConfigOptions}
-       * @description Config options for CLI tool
-       * @default { projectName: "", projectDescription: "", transpileOptions: { transpile: false }, libraryOptions: { isLibrary: false, isForMonorepo: false }, dom: false }
-       *
-       * This is the main action of the CLI tool
-       */
-      const config: ConfigOptions = {
-        projectName: "",
-        projectDescription: "",
-        transpileOptions: {
-          transpile: false,
-        },
-        libraryOptions: {
-          isLibrary: false,
-          isForMonorepo: false,
-        },
-        dom: false,
-      };
+function main() {
+  const program = new Command();
+  program
+    .name("ts-init")
+    .description("Yet another typescript tool.")
+    .version("1.0.0")
+    .option("-n, --name <name>", "Project name")
+    .option("-d, --description <description>", "Project description")
+    .option(
+      "-t, --transpile <transpile>",
+      "Do you want to transpile your project?"
+    )
+    .option("-o, --output-dir <outputDir>", "What is your output directory?")
+    .option("-l, --library <library>", "Are you building a library?")
+    .option("-m, --monorepo <monorepo>", "Is the library for a monorepo?")
+    .option("-b, --dom <dom>", "Will your app be running in the browser?")
 
-      /** Checks if the user has provided a project name */
-      if (!options.name) {
-        const nameAnswer = await inquirer.prompt([
-          {
-            type: "input",
-            name: "projectName",
-            message: "What is your project name?",
-            default: "ts-app",
+    .action(async (options) => {
+      try {
+        /**
+         * @type {ConfigOptions}
+         * @description Config options for CLI tool
+         * @default { projectName: "", projectDescription: "", transpileOptions: { transpile: false }, libraryOptions: { isLibrary: false, isForMonorepo: false }, dom: false }
+         *
+         * This is the main action of the CLI tool
+         */
+
+        const config: ConfigOptions = {
+          projectName: "",
+          projectDescription: "",
+          transpileOptions: {
+            transpile: false,
           },
-        ]);
-        config.projectName = nameAnswer.projectName;
-      } else {
-        config.projectName = options.name;
-      }
-
-      /** Checks if the user has provided a project description */
-      if (!options.description) {
-        const descriptionAnswer = await inquirer.prompt([
-          {
-            type: "input",
-            name: "projectDescription",
-            message: "What is your project description?",
-            default: "Typescript app created with ts-init",
+          libraryOptions: {
+            isLibrary: false,
+            isForMonorepo: false,
           },
-        ]);
-        config.projectDescription = descriptionAnswer.projectDescription;
-      } else {
-        config.projectDescription = options.description;
-      }
+          dom: false,
+        };
 
-      /** Checks if the user has provided a transpile option */
-      if (!options.transpile) {
-        const isTranspiled = await inquirer.prompt([
-          {
-            type: "confirm",
-            name: "transpile",
-            message: "Do you want to transpile your project?",
-            default: true,
-          },
-        ]);
-
-        // If transpiled
-        // Ask for extra transpile options
-        if (isTranspiled.transpile) {
-          const transpiledOptions = await inquirer.prompt([
+        /** Checks if the user has provided a project name */
+        if (!options.name) {
+          const nameAnswer = await inquirer.prompt([
             {
               type: "input",
-              name: "outputDir",
-              message: "What is your output directory?",
-              default: "dist",
+              name: "projectName",
+              message: "What is your project name?",
+              default: "ts-app",
             },
+          ]);
+          config.projectName = nameAnswer.projectName;
+        } else {
+          config.projectName = options.name;
+        }
+
+        /** Checks if the user has provided a project description */
+        if (!options.description) {
+          const descriptionAnswer = await inquirer.prompt([
+            {
+              type: "input",
+              name: "projectDescription",
+              message: "What is your project description?",
+              default: "Typescript app created with ts-init",
+            },
+          ]);
+          config.projectDescription = descriptionAnswer.projectDescription;
+        } else {
+          config.projectDescription = options.description;
+        }
+
+        /** Checks if the user has provided a transpile option */
+        if (!options.transpile) {
+          const isTranspiled = await inquirer.prompt([
             {
               type: "confirm",
-              name: "isLibrary",
-              message: "Are you building a library?",
-              default: false,
+              name: "transpile",
+              message: "Do you want to transpile your project?",
+              default: true,
             },
           ]);
 
-          if (transpiledOptions.isLibrary) {
-            const monorepoInquery = await inquirer.prompt([
+          /**
+           * If transpiled is true, ask for extra transpile options
+           * We need to know the output directory and if the project is a library
+           * If the project is a library, we need to know if it's for a monorepo
+           */
+          if (isTranspiled.transpile) {
+            const transpiledOptions = await inquirer.prompt([
+              {
+                type: "input",
+                name: "outputDir",
+                message: "What is your output directory?",
+                default: "dist",
+              },
               {
                 type: "confirm",
-                name: "isForMonorepo",
-                message: "Is the library for a monorepo?",
+                name: "isLibrary",
+                message: "Are you building a library?",
                 default: false,
               },
             ]);
-            config.libraryOptions.isForMonorepo = monorepoInquery.isForMonorepo;
+
+            /**
+             * If the project is a library, ask if it's for a monorepo
+             */
+            if (transpiledOptions.isLibrary) {
+              const monorepoInquery = await inquirer.prompt([
+                {
+                  type: "confirm",
+                  name: "isForMonorepo",
+                  message: "Is the library for a monorepo?",
+                  default: false,
+                },
+              ]);
+              config.libraryOptions.isForMonorepo =
+                monorepoInquery.isForMonorepo;
+            }
+
+            // Set the transpile options
+            config.transpileOptions.transpile = true;
+            config.transpileOptions.outputDir = transpiledOptions.outputDir;
+            config.libraryOptions.isLibrary = transpiledOptions.isLibrary;
           }
-
-          config.transpileOptions.transpile = true;
-          config.transpileOptions.outputDir = transpiledOptions.outputDir;
-
-          config.libraryOptions.isLibrary = transpiledOptions.isLibrary;
         }
-      } else {
-        config.transpileOptions.transpile = options.transpile;
-        config.transpileOptions.outputDir = options.outputDir;
-        config.libraryOptions.isLibrary = options.library;
-        config.libraryOptions.isForMonorepo = options.monorepo;
-      }
 
-      // DOM options
-      if (!options.dom) {
-        const domAnswer = await inquirer.prompt([
-          {
-            type: "confirm",
-            name: "dom",
-            message: "Will your app be running in the browser?",
-            default: false,
+        /**
+         * DOM options
+         * Ask if the project will be running in the browser
+         * When running in the browser, there are some types that need to be added to the tsconfig.json file
+         * These are the types for DOM and DOM.Iterable
+         */
+        if (!options.dom) {
+          const domAnswer = await inquirer.prompt([
+            {
+              type: "confirm",
+              name: "dom",
+              message: "Will your app be running in the browser?",
+              default: false,
+            },
+          ]);
+          config.dom = domAnswer.dom;
+        } else {
+          config.dom = options.dom;
+        }
+
+        /**
+         * Create the ts config file
+         * Create tsconfig.json
+         * install dependencies
+         */
+        createTsConfigFile({
+          compilerOptions: {
+            /* Base Options: */
+            esModuleInterop: true,
+            skipLibCheck: true,
+            target: "es2022",
+            allowJs: true,
+            resolveJsonModule: true,
+            moduleDetection: "force",
+            isolatedModules: true,
+            verbatimModuleSyntax: true,
+            strict: true,
+            noUncheckedIndexedAccess: true,
+            noImplicitOverride: true,
+            /** Custom Options: */
+            ...(config.transpileOptions.transpile
+              ? {
+                  /* If transpiling with TypeScript, tsc */
+                  outDir: config.transpileOptions.outputDir,
+                  module: "NodeNext",
+                  sourceMap: true,
+                }
+              : {
+                  /* If NOT transpiling with TypeScript: */
+                  module: "preserve",
+                  noEmit: true,
+                }),
+            ...(config.libraryOptions.isLibrary && {
+              /** If building a library */
+              declaration: true,
+            }),
+            ...(config.libraryOptions.isForMonorepo && {
+              /** If building a library for a monorepo */
+              composite: true,
+              declarationMap: true,
+            }),
+            ...(config.dom
+              ? {
+                  lib: ["es2022", "DOM", "DOM.Iterable"],
+                }
+              : {
+                  lib: ["es2022"],
+                }),
           },
-        ]);
-        config.dom = domAnswer.dom;
-      } else {
-        config.dom = options.dom;
+        });
+
+        /** Initilize the npm app */
+        initNpmApp(config.projectName, config.projectDescription);
+      } catch (error) {
+        console.error(chalk.red("An error occurred:"), error);
       }
+    });
 
-      /**
-       * Create the ts config file
-       * Create tsconfig.json
-       * install dependencies
-       *
-       */
+  program.parse();
+}
 
-      // Create tsconfig.json
-      createTsConfigFile({
-        compilerOptions: {
-          /* Base Options: */
-          esModuleInterop: true,
-          skipLibCheck: true,
-          target: "es2022",
-          allowJs: true,
-          resolveJsonModule: true,
-          moduleDetection: "force",
-          isolatedModules: true,
-          verbatimModuleSyntax: true,
-          strict: true,
-          noUncheckedIndexedAccess: true,
-          noImplicitOverride: true,
-          /** Custom Options: */
-          ...(config.transpileOptions.transpile
-            ? {
-                /* If transpiling with TypeScript, tsc */
-                outDir: config.transpileOptions.outputDir,
-                module: "NodeNext",
-                sourceMap: true,
-              }
-            : {
-                /* If NOT transpiling with TypeScript: */
-                module: "preserve",
-                noEmit: true,
-              }),
-          ...(config.libraryOptions.isLibrary && {
-            /** If building a library */
-            declaration: true,
-          }),
-          ...(config.libraryOptions.isForMonorepo && {
-            /** If building a library for a monorepo */
-            composite: true,
-            declarationMap: true,
-          }),
-          ...(config.dom
-            ? {
-                lib: ["es2022", "DOM", "DOM.Iterable"],
-              }
-            : {
-                lib: ["es2022"],
-              }),
-        },
-      });
-
-      /** Initilize the npm app */
-      initNpmApp(config.projectName, config.projectDescription);
-    } catch (error) {
-      console.error(chalk.red("An error occurred:"), error);
-    }
-  });
-
-program.parse();
+main();
--- package/scripts.ts
+++ package/scripts.ts
@@ -5,11 +5,27 @@ import { execSync } from "child_process";
 import chalk from "chalk";
 
 export function initNpmApp(name: string, description: string) {
+  /**
+   * @param {string} name - The name of the project
+   * @param {string} description - The description of the project
+   * @returns {void}
+   * @description
+   * This funtion creates a npm app
+   * It runs npm init and then modifies the package.json
+   * Then it installs typescript and @types/node as dev dependencies
+   */
   try {
+    // Indicate that we are initializing npm project
+    console.log(chalk.green("Initializing npm project..."));
     // Run default init
     execSync("npm init -y", { stdio: "ignore" });
 
-    // Read and modify package.json
+    /**
+     * Read and modify package.json
+     * Set the name and description
+     * Set type to module
+     * Install typescript and @types/node as dev dependencies
+     */
     const pkgPath = path.join(process.cwd(), "package.json");
     const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
     pkg.name = name;
@@ -30,9 +46,21 @@ export function initNpmApp(name: string, description: string) {
 }
 
 export function createTsConfigFile(tsConfig: TsConfig) {
-  fs.writeFileSync(
-    path.join(process.cwd(), "tsconfig.json"),
-    JSON.stringify(tsConfig, null, 2)
-  );
-  console.log(chalk.green("Created tsconfig.json"));
+  /**
+   * @param {TsConfig} tsConfig - The tsconfig.json file
+   * @returns {void}
+   * @description
+   * This function creates a tsconfig.json file
+   * Simply writes the tsconfig.json file to the current working directory base on the provided tsConfig object
+   * View the tsConfig object type in types.ts
+   */
+  try {
+    fs.writeFileSync(
+      path.join(process.cwd(), "tsconfig.json"),
+      JSON.stringify(tsConfig, null, 2)
+    );
+    console.log(chalk.green("Created tsconfig.json"));
+  } catch (err) {
+    console.error(chalk.red("An error occurred:"), err);
+  }
 }
